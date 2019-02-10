880341530e91aeee3c5c1082bfcbf8f6f1b6faf1
Kamila de fatima santos oliveira <32311268+Kamilahsantos@users.noreply.github.com>
2019-02-10 16:07:26 -0200

    Update RecomendadorBuilder.java

--- src/main/java/br/com/kamila/RecomendadorBuilder.java
+++ src/main/java/br/com/kamila/RecomendadorBuilder.java
@@ -10,10 +10,10 @@ import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
-//aqui connstruimos nosso recomendador
+
 public class RecomendadorBuilder implements RecommenderBuilder {
     public Recommender buildRecommender(DataModel model) throws TasteException {
-        //ele irá se basear na similaridade entre os usuários para contruir recomendações.
+        
         UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
         UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
         UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
